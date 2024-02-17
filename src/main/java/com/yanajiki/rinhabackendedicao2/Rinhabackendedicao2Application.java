8bca7b36ac9032a5c9bd2cca3bbd862f0b91b3f5
yan <yanajiki2010@hotmail.com>
2024-02-17 20:49:20 -0300

    first commit

---
+++ src/main/java/com/yanajiki/rinhabackendedicao2/Rinhabackendedicao2Application.java
@@ -0,0 +1,13 @@
+package com.yanajiki.rinhabackendedicao2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rinhabackendedicao2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Rinhabackendedicao2Application.class, args);
+	}
+
+}
