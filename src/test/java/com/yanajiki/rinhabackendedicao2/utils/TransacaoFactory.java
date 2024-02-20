ac57f489bcf6465cde0e4d6b3149acc437c439d8
yan <yanajiki2010@hotmail.com>
2024-02-20 14:21:45 -0300

    test: factory para casos de testes

--- src/test/java/com/yanajiki/rinhabackendedicao2/utils/TransacaoFactory.java
+++ src/test/java/com/yanajiki/rinhabackendedicao2/utils/TransacaoFactory.java
@@ -5,10 +5,10 @@ import com.yanajiki.rinhabackendedicao2.model.Transacao;
 public class TransacaoFactory {
 
     public static Transacao geraTransacaoDebitoDe1000() {
-        return new Transacao(1000, 'D', "DESCRICAO");
+        return new Transacao(1000, 'd', "DESCRICAO");
     }
 
     public static Transacao geraTransacaoCreditoDe1000() {
-        return new Transacao(1000, 'C', "DESCRICAO");
+        return new Transacao(1000, 'c', "DESCRICAO");
     }
 }
